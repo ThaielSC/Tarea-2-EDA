205fc1548b62869c92a47929c56606bcfd02efda
Thaiel Santiago <tmsantiago1@miuandes.cl>
2025-09-19 14:34:05 -0300

    chore: Sync deleted files

--- include/utils/data_loader.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef DATA_LOADER_HPP
-#define DATA_LOADER_HPP
-
-#include <string>
-#include <vector>
-
-std::vector<std::string> load_data(const std::string& filename);
-
-#endif  // DATA_LOADER_HPP
