e893de660d25df8e07446c82c9fa8309c11f5113
Thaiel Santiago <tmsantiago1@miuandes.cl>
2025-10-02 18:01:17 -0300

    Feature: Implement LinkedList

--- include/structures/linkedList.hpp
+++ include/structures/linkedList.hpp
@@ -1,5 +1,22 @@
 #ifndef LINKEDLIST_HPP
 #define LINKEDLIST_HPP
 
+#include "structures/node.hpp"
 
-#endif // LINKEDLIST_HPP
+class LinkedList {
+private:
+  Node *head;
+
+public:
+  LinkedList();
+  void insertFirst(int);
+  void insertLast(int);
+  void removeFirst();
+  void removeLast();
+  void remove(int);
+  void removeAll();
+  Node *find(int);
+  void print();
+  virtual ~LinkedList();
+};
+#endif
